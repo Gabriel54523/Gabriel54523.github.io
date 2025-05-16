816c169b809e28d7ac9c6be16fa40c3331c4b91e
Gabriel54523 <gabriel5453456@gmail.com>
2025-05-15 20:17:31 -0500

    eliminamos codigo

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
- document.getElementById("searchBar").addEventListener("input", function () {
-  const query = this.value.toLowerCase();
-  const articles = document.querySelectorAll(".news-card");
-
-  articles.forEach(article => {
-    const text = article.innerText.toLowerCase();
-    if (text.includes(query)) {
-      article.style.display = "block";
-    } else {
-      article.style.display = "none";
-    }
-  });
-});
+ 
